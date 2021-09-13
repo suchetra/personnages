8ed04ccfef9aeb07edc408c7194e80ae5b3cc360
suchetra <nicolas.tan@le-campus-numerique.fr>
2021-09-13 15:26:36 +0200

    number of characters instanciated

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+// On crée une pile d'autoload avec le spl_autoload
+spl_autoload_register('requireClass');
+// On crée une fonction pour faire les require de Classe
+function requireClass($classe){
+require $classe . '.php';
+}
+include 'usages.php'; // On inclut les usages.
+
+
+
+public static $_nbObjects = 0; // attribut de classe
+const FORCE_PETITE = 20; // constante de classe
+public static function nbObjects(){ // méthode de classe
+self::$_nbObjects; // accès à un attribut de classe
+self::FORCE_PETITE // accès à un attribut de classe
+Personnage::FORCE_MOYENNE // accès à un attribut de classe
+public function __toString() // méthode magique
+echo($objet); // <=> echo $objet->__toString()
+?>
--- classes.php
+++ classes.php
@@ -1,18 +1,56 @@
 <?php
 class Personnage
 {
+
+    // $_nombre : variable de classe : static, initialisée à 0
+    public static $nbObjects = 0;
+
+    // etc.
+    // function static
+    public static function nbObjects(){
+    // on accede à la variable static par self et ::
+    // self represente la classe, on pourrait metre Personnage
+    return self::$nbObjects;
+    }
+
+//     public function __construct($force, $degats) {
+//     // etc.
+//     // on compte le nombre d'instances de la classe
+    
+// }
     public $_vie;
+
+    // private $force;
+    // // Déclarations des constantes en rapport avec la force.
+    // const FORCE_PETITE = 20;
+    // const FORCE_MOYENNE = 50;
+    // const FORCE_GRANDE = 80;
+
+    
     public $_force = 9999; // La force du personnage, par défaut 50
     public $_localisation = 9999; // Sa localisation
     public $_experience = 9999; // Son expérience
     public $_degats; // Ses dégâts
 
     public function __construct($_vie, $_force, $_localisation, $_experience, $_degats){
-        $this->_vie = $_vie;
-        $this->_force = $_force;
-        $this->_localisation = $_localisation;
-        $this->_experience = $_experience;
-        $this->_degats = $_degats;
+    $this->_vie = $_vie;
+
+
+    // le setter vérifie l’intégrité des données fournies
+    // On vérifie que $force vaut « FORCE_PETITE »,
+    // ou « FORCE_MOYENNE », ou « FORCE_GRANDE ».
+    // if (in_array($force, [self::FORCE_PETITE,
+    // self::FORCE_MOYENNE, self::FORCE_GRANDE])) {
+    // $this->force = $force;
+    // }
+    // else{ // sinon on donne une petite force par défaut !
+    // $this->force = self::FORCE_PETITE;
+    
+    $this->_force = $_force;
+    $this->_localisation = $_localisation;
+    $this->_experience = $_experience;
+    $this->_degats = $_degats;
+    self::$nbObjects++;
     }
 
     public function parler($something){
@@ -46,5 +84,9 @@ class Personnage
         // $perso2[$_vie] - $perso1[$_force];
         $cible->_vie -= $this->_force;
     }
+
+
+    
+    
 
 }
